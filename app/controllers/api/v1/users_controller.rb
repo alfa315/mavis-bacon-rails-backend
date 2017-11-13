d457f3b702f6162e4c16b979276febb236880f9f
jonopens <jonopenshaw@gmail.com>
2017-11-13 14:56:13 -0500

    fixed issue with create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,18 +11,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.find_or_create_by(user_params)
+    @user = User.find_or_create_by(username: params[:username])
+
     if @user.save
       render json: @user
     else
       render json: {error: @user.errors.full_messages}, status: 422
     end
-  end
-
-  private
-
-  def user_params
-   params.require(:user).permit(:username)
   end
 
 end
