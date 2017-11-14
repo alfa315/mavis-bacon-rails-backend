1b0ffa2a5c699d0ffd5fce58a55533cdbc4ee262
github username <github email address>
2017-11-14 11:02:09 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-jon = User.create(username: 'colostomy-bag')
+jon = User.create(username: 'jonopens')
+al = User.create(username: 'alfa315')
+ashe = User.create(username: 'asheee')
 
-game_one = Game.create(user_id: 1, wpm: 10000, game_won: true)
+game_one = Game.create(user_id: 1, wpm: 60, game_won: true)
+game_two = Game.create(user_id: 2, wpm: 58, game_won: true)
+game_three = Game.create(user_id: 3, wpm: 5, game_won: true)
+game_four = Game.create(user_id: 1, wpm: 100, game_won: true)
+game_five = Game.create(user_id: 2, wpm: 1000, game_won: true)
+game_six = Game.create(user_id: 3, wpm: 0, game_won: true)
